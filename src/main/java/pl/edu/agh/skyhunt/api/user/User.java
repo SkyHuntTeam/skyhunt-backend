0c4fab7f1b413e9e65b455b2c15e3a62192060c0
M1ch4lx <m.kowalczyk360@gmail.com>
2023-11-17 01:48:29 +0100

    Changed user schema name because "user" is restricted in postgresql

--- src/main/java/pl/edu/agh/skyhunt/api/user/User.java
+++ src/main/java/pl/edu/agh/skyhunt/api/user/User.java
@@ -3,10 +3,12 @@ package pl.edu.agh.skyhunt.api.user;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Data;
 
 @Entity
 @Data
+@Table(name = "portal_user")
 public class User {
 
     @Id
